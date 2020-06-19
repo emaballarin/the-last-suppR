bc1dd69b7c05c80bae1e8a92ea898bb1a38d2f75
wilsongefferson <pierpa.m.96live.it>
2020-06-19 14:12:34 +0200

    Started DAAG ex 4.5

--- src/DAAG_04_05.R
+++ src/DAAG_04_05.R
@@ -1,1 +1,16 @@
- 
+oldpar <- par(mfrow=c(2,2))
+
+tenfold1000 <- rep(1:10, rep(1000,10))
+
+boxplot(split(rnorm(1000*10), tenfold1000), ylab="normal - 1000")
+boxplot(split(rt(1000*10, 7), tenfold1000),
+        ylab=expression(t[7]*" - 1000"))
+
+tenfold100 <- rep(1:10, rep(100, 10))
+
+boxplot(split(rnorm(100*10), tenfold100), ylab="normal - 100")
+boxplot(split(rt(100*10, 7), tenfold100),
+        ylab=expression(t[7]*" - 100")) 
+
+par(oldpar)
+
